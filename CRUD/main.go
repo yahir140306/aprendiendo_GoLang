8fc8f1df76ac29fc365fe9de40df4fdd8326f9f1
yahir140306 <joseyahirhernandez11@gmail.com>
2025-04-24 23:08:52 -0600

    add functions

--- CRUD/main.go
+++ CRUD/main.go
@@ -29,21 +29,47 @@ var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 func main() {
 	http.HandleFunc("/", Inicio)
 	http.HandleFunc("/crear", Crear)
+	http.HandleFunc("/insertar", Insertar)
 
 	log.Println("Servidor corriendo")
 	fmt.Println("Servidor corriendo")
 	http.ListenAndServe(":8080", nil)
 }
 
+type Empleado struct {
+	Id     int
+	Nombre string
+	Correo string
+}
+
 func Inicio(w http.ResponseWriter, r *http.Request) {
 
 	conexionEstablecida := conexionBD()
-	insertarRegistro, err := conexionEstablecida.Prepare("INSERT INTO empleados(nombre,correo) VALUES ('oscar', 'correo@gmail.com')")
+	registros, err := conexionEstablecida.Query("SELECT * FROM empleados")
 
 	if err != nil {
 		panic(err.Error())
 	}
-	insertarRegistro.Exec()
+
+	empleado := Empleado{}
+	arregloEmpleado := []Empleado{}
+
+	for registros.Next() {
+		var id int
+		var nombre, correo string
+		err = registros.Scan(&id, &nombre, &correo)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		empleado.Id = id
+		empleado.Nombre = nombre
+		empleado.Correo = correo
+
+		arregloEmpleado = append(arregloEmpleado, empleado)
+	}
+
+	fmt.Println(arregloEmpleado)
 
 	// fmt.Fprintf(w, "Hola Mundo")
 	plantillas.ExecuteTemplate(w, "inicio", nil)
@@ -51,4 +77,21 @@ func Inicio(w http.ResponseWriter, r *http.Request) {
 
 func Crear(w http.ResponseWriter, r *http.Request) {
 	plantillas.ExecuteTemplate(w, "crear", nil)
+}
+
+func Insertar(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nombre := r.FormValue("nombre")
+		correo := r.FormValue("correo")
+
+		conexionEstablecida := conexionBD()
+		insertarRegistro, err := conexionEstablecida.Prepare("INSERT INTO empleados(nombre,correo) VALUES (?,?)")
+
+		if err != nil {
+			panic(err.Error())
+		}
+		insertarRegistro.Exec(nombre, correo)
+
+		http.Redirect(w, r, "/", 301)
+	}
 }
