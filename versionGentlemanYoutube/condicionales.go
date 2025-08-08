4a38d6c4f17e2865ca39d87cd66f720f51d70da5
yahir140306 <joseyahirhernandez11@gmail.com>
2025-08-07 23:30:53 -0600

    refactor condicionales function to improve clarity and structure

--- versionGentlemanYoutube/condicionales.go
+++ versionGentlemanYoutube/condicionales.go
@@ -3,10 +3,59 @@ package main
 import "fmt"
 
 func condicionales() {
-	fmt.Println("para que no molesta")
-	edad := 18
-	if edad >= 18 {
-		fmt.Printf("Eres mayor de edad")
+
+	// condicionales
+	edad := 20
+	if edad < 18 {
+		fmt.Printf("Eres menor de edad")
+		return
 	}
 
+	fmt.Println("Eres mayor de edad")
+
+	// ciclos
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("Iteración:", i)
+		fmt.Printf("Iteración: %d\n", i)
+	}
+
+	// while
+	n := 0
+	for n < 3 {
+		fmt.Printf("Iteración: %d\n", n)
+		n++
+	}
+
+	// infinito
+	n = 0
+	for {
+		n++
+		if n == 5 {
+			continue
+		}
+
+		fmt.Printf("n en bucle infinito: %d\n", n)
+
+		if n >= 7 {
+			break
+		}
+	}
+
+	// range
+	slice := []string{"uno", "dos", "tres"}
+	for index, value := range slice {
+		fmt.Printf("Indice: %d, Valor: %s\n", index, value)
+	}
+
+	// switch
+	valor := 2
+	switch valor {
+	case 1:
+		fmt.Println("Es 1")
+	case 2:
+		fmt.Printf("Es 2")
+	default:
+		fmt.Printf("No es 1 ni 2")
+	}
 }
