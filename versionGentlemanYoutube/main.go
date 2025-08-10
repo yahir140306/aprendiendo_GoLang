d803d45bd646269148de7ed6f9255538f89ade0e
yahir140306 <joseyahirhernandez11@gmail.com>
2025-08-09 22:34:27 -0600

    add go.mod file and initialize module for versionGentlemanYoutube

--- versionGentlemanYoutube/main.go
+++ versionGentlemanYoutube/main.go
@@ -1,50 +1,46 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	// Enterosa
-	entero := 10
-	decimal := 3.14
-	suma := entero + int(decimal)
-	fmt.Println(suma)
+	// entero := 10
+	// decimal := 3.14
+	// suma := entero + int(decimal)
+	// fmt.Println(suma)
 
-	// Texto
+	// // Texto
+	// mensaje := "Hola, "
+	// concatenado := mensaje + "Juanchis"
+	// fmt.Println(concatenado)
 
-	mensaje := "Hola, "
-	concatenado := mensaje + "Juanchis"
-	fmt.Println(concatenado)
+	// // bool
+	// esVerdadero := true
+	// fmt.Println(esVerdadero)
 
-	// bool
-	esVerdadero := true
-	fmt.Println(esVerdadero)
+	// // Arrays y Slices
+	// arrayFijo := [3]int{1, 2, 3}
+	// capacidad := cap(arrayFijo)
+	// fmt.Println(capacidad)
 
-	// Arrays y Slices
-	arrayFijo := [3]int{1, 2, 3}
-	capacidad := cap(arrayFijo)
-	fmt.Println(capacidad)
+	// sliceVariable := []int{4, 5, 6}
+	// sliceVariable = append(sliceVariable, 7)
 
-	sliceVariable := []int{4, 5, 6}
-	sliceVariable = append(sliceVariable, 7)
+	// // Mapas
+	// diccionario := map[string]int{
+	// 	"clave1": 1,
+	// 	"clave2": 2,
+	// }
 
-	// Mapas
-	diccionario := map[string]int{
-		"clave1": 1,
-		"clave2": 2,
-	}
+	// fmt.Println(diccionario)
 
-	fmt.Println(diccionario)
+	// // Structs
+	// type Persona struct {
+	// 	Nombre string
+	// 	Edad   int
+	// }
 
-	// Structs
-	type Persona struct {
-		Nombre string
-		Edad   int
-	}
+	// persona := Persona{Nombre: "Juanchis", Edad: 30}
+	// fmt.Println(persona)
 
-	persona := Persona{Nombre: "Juanchis", Edad: 30}
-	fmt.Println(persona)
-
-	condicionales()
+	// condicionales()
+	funciones()
 }
