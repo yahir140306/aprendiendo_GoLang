d0f42f94658d39565ba240d4a7eb2e970ea0820a
yahir140306 <joseyahirhernandez11@gmail.com>
2025-08-16 23:50:01 -0600

    add: implement concurrency functions with channels and mutex example

--- versionGentlemanYoutube/interfaces.go
+++ versionGentlemanYoutube/interfaces.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"sync"
+	"time"
 )
 
 // las interfaces no se implementan al menos manualmente
@@ -35,7 +37,61 @@ func interfaces() {
 
 // go => GoRoutine
 // GoRoutine => es un hilo de ejecucion ligero virtual
+// channel => para comunicar cosas entre GoRoutine
+
+func decirHola(canal chan<- string) {
+	time.Sleep(1 * time.Second)
+	canal <- "Hola desde la GoRoutine"
+}
+
+func imprimirMensaje(canal <-chan string) {
+	fmt.Println("Esperando mensaje...")
+	msg := <-canal
+	fmt.Println(msg)
+}
 
 func concurrencia() {
+	canal := make(chan string)
+	go decirHola(canal)
+	imprimirMensaje(canal)
 
+	canal2 := make(chan int)
+	go func() {
+		for i := 0; i < 5; i++ {
+			canal2 <- i
+		}
+		close(canal2)
+	}()
+
+	for num := range canal2 {
+		fmt.Println("Numero recibido:", num)
+	}
+
+	// Ejemplo Mutex
+	var contador int
+	var mu sync.RWMutex
+
+	// writer
+	go func() {
+		for i := 0; i < 5; i++ {
+			mu.Lock()
+			contador++
+			mu.Unlock()
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	// reader
+	for i := 0; i < 3; i++ {
+		go func(id int) {
+			for j := 0; j < 5; j++ {
+				mu.RLock()
+				fmt.Printf("Leyendo desde la GoRoutine %d: %d\n", id, contador)
+				mu.RUnlock()
+				time.Sleep(100 * time.Millisecond)
+			}
+		}(i)
+	}
+
+	time.Sleep(2 * time.Second)
 }
